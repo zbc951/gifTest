61e434d57c3136f5301618796c3d1a1021596418
lazio@wataidesign.com <lazio@wataidesign.com>
2022-02-25 16:41:34 +0800

    111-0225-6

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".jpBtn").click(function(){
+        $(".wrap").attr("class","wrap jp");
+    })
+
+    $(".nt3Btn").click(function(){
+        $(".wrap").attr("class","wrap nt3");
+    })
+})
